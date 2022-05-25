3caf79d03f39afcc804ff7257400b5afa443c886
Leandro MV <lmeza@lsv-tech.com>
2022-05-24 19:15:53 -0500

    Creacion de la clase contador y contador screen

---
+++ lib/view/count_screen.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class Contador extends StatefulWidget {
+  const Contador({Key? key}) : super(key: key);
+
+  @override
+  State<Contador> createState() => _ContadorState();
+}
+
+class _ContadorState extends State<Contador> {
+  @override
+  Widget build(BuildContext context) {
+    const siseText =  TextStyle(fontSize: 25);
+    int count = 10 ;
+    return Scaffold( 
+      appBar: AppBar(
+          elevation: 8.0,
+          backgroundColor: Color.fromARGB(223, 37, 34, 34),
+          title: const Text('Countador'),
+        ),
+        body: Center(
+          child: Column( 
+            mainAxisAlignment: MainAxisAlignment.center,
+            
+            children:  <Widget>[
+              Text("Listo para cambiar", style:siseText ,),
+              Text('$count' ,style:siseText,)
+            ]
+          ),
+        ), 
+        floatingActionButton: FloatingActionButton(
+          onPressed:(){
+            count++;
+            showMessage();
+            },
+          child: Icon(Icons.access_alarm),
+        ) 
+        
+        
+        ); 
+  }
+
+  void showMessage(){
+    print('hi, Again');
+  }
+}
+
